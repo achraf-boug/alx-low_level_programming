a258b6ba592e1407ba72e43e54ad40e7e8891084
achraf-boug <achraf.bougadir@gmail.com>
2023-07-18 16:24:07 -0700

    Mandatory task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @n: The number to begin counting at.
+ */
+void print_to_98(int n)
+{
+	int a;
+	if (n>98)
+		a = -1;
+	else
+		a = 1;
+	
+	while(n!= 98)
+	{
+		printf("%d, ",n);
+		n+=a;
+	}
+	printf("%d",98);
+}
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for(a = 0; a < 10; a++)
+	{
+		putchar('0');
+		for(b = 1; b < 10; b++)
+		{
+			putchar(',');
+			putchar(' ');
+
+			if( (a*b) > 10 )
+				putchar(((a*b)/10) + '0');
+			else
+				putchar(' ');
+
+			putchar( (a*b)%10 + '0');
+		}
+		putchar('\n');
+	}
+}
+
