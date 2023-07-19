6dd3e7caad653c92f0ad2c445b0ffa21bed33d51
achraf-boug <achraf.bougadir@gmail.com>
2023-07-18 17:51:09 -0700

    why betty why ?

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - prints the n times table, starting with 0
+ * print_times_table - prints the n times table, starting with 0
  */
 void print_times_table(int n)
 {
