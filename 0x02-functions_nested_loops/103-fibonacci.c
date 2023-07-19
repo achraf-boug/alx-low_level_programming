fc80d0afa797120521107afedd1a9008498e510f
achraf-boug <achraf.bougadir@gmail.com>
2023-07-18 18:12:39 -0700

    advanced task 3

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of even-valued Fibonacci sequence
+ *        terms not exceeding 4000000.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long prev = 1, last = 1, sum = 0,sumeven = 0;
+	
+	while (sum < 4000000)
+	{
+		sum = last + prev;
+		prev = last;
+		last = sum;
+		if (sum % 2 == 0)
+			sumeven += sum;
+	}
+	printf("%ld\n", sumeven);
+
+	return (0);
+}
+
