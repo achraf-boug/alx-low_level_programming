68e9f5b88110a77b7edc2d7d11f95aa92772afc6
achraf-boug <achraf.bougadir@gmail.com>
2023-07-25 00:37:04 +0100

    waaaaaaaa betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,11 @@ void print_rev(char *s)
 	int len = 0;
 	int i;
 
-	for ( i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		len++;
 
-	for (i = len; i > -1; i--)
-	{
+	for (i = len-1; i > -1; i--)
 		_putchar(s[i]);
-	}
 
 	_putchar('\n');
 
