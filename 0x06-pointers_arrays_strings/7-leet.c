14bb91d6ecbbc42a2ba4cedf3d2e0056c26aa43e
achraf-boug <achraf-bougadir@gmail.com>
2023-07-27 18:09:26 -0700

    fix betty on task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,27 @@
 #include "main.h"
 /**
- * leet - function that encodes a string into 1337
+ * char *leet - function that encodes a string into 1337
  * @str: string to treat
  * Return: Always 0.
  */
 char *leet(char *str)
 {
-    char s[] = "AaEeOoTtLl";
-    int c[] = "4433007711";
-    int i, j = 0;
+	char s[] = "AaEeOoTtLl";
+	int c[] = "4433007711";
+	int i, j = 0;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        while (s[j] != '\0'){
-            if (str[i] == s[j])
-            {
-                str[i] = c[j];
-                break;
-            }
-            j++;
-        }
-        j=0;
-    }
-    return str;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		while (s[j] != '\0')
+		{
+			if (str[i] == s[j])
+			{
+				str[i] = c[j];
+				break;
+			}
+		}
+		j = 0;
+	}
+
+	return (str);
 }
