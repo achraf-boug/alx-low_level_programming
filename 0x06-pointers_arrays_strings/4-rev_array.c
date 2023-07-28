b35bbcf52ca600bae3922f8582e5d14f43189599
achraf-boug <achraf-bougadir@gmail.com>
2023-07-27 18:29:25 -0700

    Im trying to understand

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-    int i, temp;
+	int i, temp;
 
-
-    for (i = 0; (i < (n - 1 ) / 2); i++)
-    {
-        temp  = a[i];
-        a[i] = a[n - 1 - i];
-        a[n - 1 - i] = temp;
-    }
+	for (i = 0; (i < (n - 1) / 2); i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
 }
+
