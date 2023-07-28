94454c82eae2a9ed7b5ec563cf79115741624ebc
achraf-boug <achraf-bougadir@gmail.com>
2023-07-27 18:01:54 -0700

    Correct Betty on task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,25 +6,30 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
-    int c;
-    
-    c = (int) str[0];
-    if (str[0] >= 97 & str[0] <= 122)
-        str[0] = (char) (c - 32);
+	int i = 0;
+	int c;
 
-    while (str[i] !='\0')
-    {
-        if (str[i] == '\t' | str[i] == '\n' | str[i] == ' ' | str[i] == ',' | str[i] == ';' | str[i] == '.' | str[i] == '!' | str[i] == '?' | str[i] == '"' | str[i] == '(' | str[i] == ')' | str[i] == '{' | str[i] == '}')
-        {
-            c = (int) str[i+1];
-            if (str[i+1] >= 97 & str[i+1] <= 122){
-                str[i+1] = (char) (c - 32);
-                i++;
-            }
-        }
-        i++;
-    }
-    
-    return str;
+	c = (int) str[0];
+	if (str[0] >= 97 & str[0] <= 122)
+		str[0] = (char) (c - 32);
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] == '\t') | (str[i] == '\n') | (str[i] == ' ') |
+			(str[i] == ',') | (str[i] == ';') | (str[i] == '.') |
+			(str[i] == '!') | (str[i] == '?') | (str[i] == '"') |
+			(str[i] == '(') | (str[i] == ')') | (str[i] == '{') |
+			(str[i] == '}'))
+		{
+			c = (int) str[i + 1];
+			if (str[i + 1] >= 97 & str[i + 1] <= 122)
+			{
+				str[i + 1] = (char) (c - 32);
+				i++;
+			}
+		}
+		i++;
+	}
+
+	return (str);
 }
