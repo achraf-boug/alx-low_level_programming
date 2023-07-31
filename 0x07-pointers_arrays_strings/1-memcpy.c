55756d24deb7c5bd8ccb236f2d166f1c6d94e973
achraf-boug <achraf-bougadir@gmail.com>
2023-07-31 14:57:39 -0700

    First 3 tasks

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: memory for storage
+ * @src: memory to copy
+ * @n: number of bytes to cpy
+ *
+ * Return: destination memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; (src[i] != '\0') && (i < n + 1); i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+	return (dest);
+}
+
