a9cf87d49ed48e1117016edd2d8cace32c81cf16
achraf-boug <achraf-bougadir@gmail.com>
2023-07-31 15:17:39 -0700

    It may work now

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; (src[i] != '\0') && (i < n + 1); i++)
+	for (i = 0; (src[i] != '\0') && (i < n); i++)
 		dest[i] = src[i];
 
-	dest[i] = '\0';
 	return (dest);
 }
 
