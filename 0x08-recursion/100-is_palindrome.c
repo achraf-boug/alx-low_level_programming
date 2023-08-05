6346cbd90c748f1b50d09d3cb21bf1b16faa18dc
achraf-boug <achraf-bougadir@gmail.com>
2023-08-04 17:00:26 -0700

    create using rc instead of rcs

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * is_equal - checks if two characters are equal
+ * @a: character
+ * @b: character
+ * Return: 1 if equals, 0 if not
+ */
+int is_equal(char *s, int start, int end)
+{
+
+}
+/**
+ * is_palindrome - function that checks if a string is palindrome
+ * @s: string
+ * Return: 1 if True, 0 if False
+ */
+int is_palindrome(char *s)
+{
+	int len;
+	int flag;
+	int start = 0;
+
+	for (len = 0; s[len] != '\0'; len++)
+		continue;
+
+
