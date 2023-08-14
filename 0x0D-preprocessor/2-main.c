1091a69a78214b0aaa5d44196701555c02964c8b
achraf-boug <achraf-bougadir@gmail.com>
2023-08-14 08:36:59 -0700

    Try fixing task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,12 @@
 /**
  * main - returns file name
  */
-void main(void)
+int main(void)
 {
 	char *filename = __FILE__;
 	int i;
 
 	for (i = 0; filename[i] != '\0'; i++)
 		_putchar(filename[i]);
+	return (0);
 }
