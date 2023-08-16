3d0a4c54fde287afd586f434e28f347431939f8d
achraf-boug <achraf-bougadir@gmail.com>
2023-08-16 14:52:49 -0700

    Handling errors in function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,11 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
 	if (array == NULL || action == NULL || size == 0)
 		return;
 
 	for (i = 0; i < size; i++)
-		array[i] = action(array[i]);
+		action(array[i]);
 }
