433ee49049be2e28af0e158b009e0fd7378a3a48
achraf-boug <achraf-bougadir@gmail.com>
2023-08-16 16:27:08 -0700

    test the following solution

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -15,13 +15,10 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	for (i = 0; ops[i].op != NULL; i++)
-	{
-		if (ops[i].op[0] == s[0])
-			return (ops[i].f);
-	}
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
-	return (0);
+	return (ops[i].f);
 }
