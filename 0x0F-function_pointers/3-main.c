e1c3f91c9cf78bf63bdc762d65d2dce27f4e8c72
achraf-boug <achraf-bougadir@gmail.com>
2023-08-16 16:43:32 -0700

    for real ?

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*op == '/' || *op == '%') && b == 0)
+	if ((op[0] == '/' || op[0] == '%') && b == 0)
 	{
 		printf("Error");
 		exit(100);
