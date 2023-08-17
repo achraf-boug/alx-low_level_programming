6aaf2cd636e04bf1b63efa4225f4f69754ab1dcf
achraf-boug <achraf-bougadir@gmail.com>
2023-08-17 13:20:03 -0700

    push final task

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything
+ * @format: list of types of argumuents
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str;
+	va_list list;
+	int flag = 0;
+
+	va_start(list, format);
+	while (format[i] != '\0' && format != NULL)
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(list, int));
+				flag = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(list, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", (float) va_arg(list, double));
+				flag = 0;
+				break;
+			case 's':
+				flag = 0;
+				str = va_arg(list, char *);
+				if (str == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+			default:
+				flag = 1;
+				break;
+		}
+
+		if (flag == 0 && format[i+1] != '\0')
+			printf(", ");
+
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
