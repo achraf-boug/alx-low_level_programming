420fa878948b733b1e614054f2b1bc0faab73468
achraf-boug <achraf-bougadir@gmail.com>
2023-08-17 11:16:28 -0700

    push task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that pronts numbers
+ * @separator: string
+ * @n: size of args
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int count;
+	va_list list;
+
+	va_start(list, n);
+
+	for (count = 1; count < n - 1; count++)
+	{
+		printf("%d", va_arg(list, int));
+		if (separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("%d\n", va_arg(list, int));
+}
